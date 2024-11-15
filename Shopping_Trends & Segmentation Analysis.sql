5cf41a7b68dbe0af9fe8c065997e21398e818196
JinLunLiang <jinlun-01@hotmail.com>
2024-11-15 22:02:01 +0800

    latest

--- Shopping_Trends & Segmentation Analysis.sql
+++ Shopping_Trends & Segmentation Analysis.sql
@@ -50,6 +50,7 @@ FROM shopping_trends
 GROUP BY customer_id, age, gender, item_purchased, category, purchase_amount_usd, location, size, color, season, review_rating, subscription_status, payment_method, shipping_type, discount_applied, promo_code_used, previous_purchases, preferred_payment_method, frequency_of_purchases
 HAVING COUNT(*) > 1;
 
+
 --Shopping Trends Analysis (Refer Tableau For Visualization : https://public.tableau.com/views/ShoppingTrendsAnalysis_17314747229240/Dashboard2?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link )
 -- 1) Top 3 Products per Category by Sales
 WITH category_sales AS (
@@ -75,16 +76,21 @@ WHERE
 ORDER BY 
     category, total_sales DESC;
 
+--Conclusion : This chart highlights that each category has its top items with fairly close sales figures, indicating diversified interest across the best-selling products within each category. Jewelry, Blouses, Shoes, and Coats are the leading items within their respective categories, but the other top items show competitive sales, suggesting that customers are interested in a variety of products rather than concentrating on a single best-seller per category.
+
 -- 2) Retention and Churn Rate by Subscription Status
 SELECT 
     subscription_status,
     COUNT(customer_id) AS Total_Customers,
-    COUNT(CASE WHEN frequency_of_purchases IN ('Bi-Weekly', 'Weekly', 'Fortnightly') THEN customer_id END) * 1.0 / COUNT(customer_id) AS Retention_Rate,
-    COUNT(CASE WHEN frequency_of_purchases IN ('Monthly', 'Annually', 'Quarterly', 'Every 3 Months') THEN customer_id END) * 1.0 / COUNT(customer_id) AS Churn_Rate
+    COUNT(CASE WHEN frequency_of_purchases IN ('Bi-Weekly', 'Weekly') THEN customer_id END) * 1.0 / COUNT(customer_id) AS Retention_Rate,
+    COUNT(CASE WHEN frequency_of_purchases IN ('Monthly', 'Quarterly', 'Annually') THEN customer_id END) * 1.0 / COUNT(customer_id) AS Churn_Rate
 FROM 
     shopping_trends
 GROUP BY 
     subscription_status;
+
+--Conclusion : 1) Subscribers have a marginally better retention rate than non-subscribers (42.74% vs. 41.38%), though the difference is minimal. This indicates that while subscription may encourage a small increase in loyalty, it is not a strong driver of retention.
+--2) Both groups show high churn rates (57-59%), indicating that a large percentage of customers—subscribed or not—are not staying engaged long-term.
 
 -- 3) Average Order Value and Purchase Frequency by Payment Method
 SELECT 
@@ -98,7 +104,9 @@ GROUP BY
 ORDER BY 
     Avg_Order_Value DESC;
 
---Market Segmentation Analysis (Refer Tableau For Visualization : https://public.tableau.com/shared/DKQXNG4PD?:display_count=n&:origin=viz_share_link )
+--Conclusion : Customers using Venmo and Credit Card tend to have the highest average order values, indicating that these methods may attract slightly higher spending customers. Cash also has a strong average order value, possibly due to in-store purchases or small but frequent transactions. However, Bank Transfer users tend to have the lowest average order values, suggesting it may attract more budget-conscious or occasional buyers.
+
+--Market Segmentation Analysis (Refer Tableau For Visualization : https://public.tableau.com/views/MarketSegmentationAnalysis_17314746907080/Dashboard1?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link )
 -- 1) Demographic Segmentation Analysis (Analyze by age group and gender to identify purchasing patterns)
 SELECT 
     CASE 
@@ -128,6 +136,8 @@ GROUP BY
 ORDER BY 
     age_group, gender;
 
+----Conclusion : The primary customer segment is males in the 36-50 and 51-65 age groups, showing the highest engagement levels by customer count. Spending behavior remains consistent across all demographics, making broad-based pricing strategies viable. There is an opportunity to increase engagement with the 18-25 and 65+ age groups by tailoring products or marketing strategies to appeal to these smaller segments.
+
 -- 2) Geographic Segmentation Analysis (Segment customers by location to understand regional differences in purchasing behavior)
 SELECT 
     location,
@@ -141,6 +151,8 @@ GROUP BY
     location
 ORDER BY 
     avg_purchase_amount DESC;
+
+--Conclusion : The visualization highlights Montana, Illinois, and California as the top-performing states in terms of sales, indicating these as key markets. States like Kansas, Florida, New Jersey and Hawaii show lower sales, suggesting opportunities for market expansion. By focusing marketing efforts on states with moderate or low sales, the business can aim to increase customer engagement and sales in underperforming regions.
 
 -- 3) Behavioral Segmentation Analysis (Analyze customer behavior based on frequency of purchases and spending habits)
 SELECT 
@@ -156,6 +168,9 @@ GROUP BY
 ORDER BY 
     avg_purchase_amount DESC;
 
+--Conclusion : 1) Quarterly and Bi-Weekly customers represent the most valuable segments, with high total spending and customer counts, making them ideal for targeted campaigns and loyalty programs.
+-- 2) The Montly, Annual and Weekly segments are smaller and spend less overall, but tailored strategies like high-value annual offers or weekly discounts could increase their spending and engagement.
+
 -- 4) Psychographic Segmentation Analysis (Analyze customer preferences based on product characteristics like category, season, and color)
 SELECT 
     category,
@@ -170,3 +185,5 @@ GROUP BY
     category, season, color
 ORDER BY 
     avg_purchase_amount DESC;
+
+--Conclusion : Accessories and Clothing are the most frequently purchased categories across seasons, with a strong preference for darker colors in Fall and Winter and lighter colors in Spring and Summer. Footwear and Outerwear are less popular overall, though they show some seasonal variation, especially in color choices for colder months.
